cf5911346cf3bc4bdd9ddafb4522c41bb0975dd0
Sabrine-Louss <loussaiefsabrine7@gmail.com>
2024-12-03 15:25:18 +0100

    front side

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {Provider} from "react-redux"
 import {BrowserRouter} from "react-router-dom"
 import store from "./redux/store"
 import axios from "axios"
-axios.defaults.baseURL= "http://localhost:5000/api"
+axios.defaults.baseURL= "https://e-commerce-server-28qf.onrender.com/api"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store} >
